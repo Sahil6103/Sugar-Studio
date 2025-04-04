81ceb26565975a9b37500c0569457378e7546e3e
sahil <sahildhandhukiya0@gmail.com>
2025-04-04 15:32:03 +0530

    sugar

--- src/main.jsx
+++ src/main.jsx
@@ -2,35 +2,35 @@ import React, { useEffect, useState, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
-// import { PreLoader } from "./components/common/PreLoader";
-// const LazyApp = React.lazy(() => import("./App"));
+import { PreLoader } from "./components/common/PreLoader";
+const LazyApp = React.lazy(() => import("./App"));
 
-// const AppWithPreloader = () => {
-//   const [isLoaded, setIsLoaded] = useState(false);
+const AppWithPreloader = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-//   useEffect(() => {
-//     const waitForAssets = async () => {
-//       // Wait for images, fonts, and full page
-//       await Promise.all([
-//         new Promise((res) => {
-//           if (document.readyState === "complete") res();
-//           else window.addEventListener("load", res);
-//         }),
-//         document.fonts ? document.fonts.ready : Promise.resolve(),
-//       ]);
-//       setIsLoaded(true);
-//     };
+  useEffect(() => {
+    const waitForAssets = async () => {
+      // Wait for images, fonts, and full page
+      await Promise.all([
+        new Promise((res) => {
+          if (document.readyState === "complete") res();
+          else window.addEventListener("load", res);
+        }),
+        document.fonts ? document.fonts.ready : Promise.resolve(),
+      ]);
+      setIsLoaded(true);
+    };
 
-//     waitForAssets();
-//   }, []);
+    waitForAssets();
+  }, []);
 
-//   return isLoaded ? (
-//     <Suspense fallback={null}>
-//       <LazyApp />
-//     </Suspense>
-//   ) : (
-//     <PreLoader />
-//   );
-// };
+  return isLoaded ? (
+    <Suspense fallback={null}>
+      <LazyApp />
+    </Suspense>
+  ) : (
+    <PreLoader />
+  );
+};
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<AppWithPreloader />);
