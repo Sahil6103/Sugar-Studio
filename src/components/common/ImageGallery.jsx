bfcc671d9fa33278e2f4d9e9c21c8194d30a4714
sahil <sahildhandhukiya0@gmail.com>
2025-04-04 15:05:31 +0530

    sugar

--- src/components/common/ImageGallery.jsx
+++ src/components/common/ImageGallery.jsx
@@ -1,44 +1,37 @@
 import React, { useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 
 export const ImageGallery = ({ images }) => {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const sectionRef = useRef();
   return (
-    <section ref={sectionRef} className=" md:px-10 lg:px-20 mb-20 mt-8 py-14">
+    <section ref={sectionRef} className="md:px-10 lg:px-20 mb-20 mt-8 py-14">
       <div className="max-w-[100rem] mx-auto columns-2 md:columns-3 lg:columns-4 gap-4">
-        {images.map((src, index) => {
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-            threshold: 0.1,
-          });
-
-          return (
-            <motion.div
-              key={index}
-              ref={ref}
-              className="mb-4 break-inside-avoid cursor-pointer"
-              layoutId={`image-${index}`}
-              whileHover={{ scale: 1.05 }}
-              onClick={() => setSelectedImage({ src, id: `image-${index}` })}
-              initial={{ scale: 0.8, opacity: 0 }}
-              animate={inView ? { scale: 1, opacity: 1 } : {}}
-              transition={{ duration: 0.5, delay: index * 0.2 }}>
-              <img
-                src={src}
-                alt={`Masonry ${index}`}
-                className="w-full rounded-lg"
-              />
-            </motion.div>
-          );
-        })}
+        {images.map((src, index) => (
+          <motion.div
+            key={index}
+            className="mb-4 break-inside-avoid cursor-pointer"
+            layoutId={`image-${index}`}
+            whileHover={{ scale: 1.03 }}
+            onClick={() => setSelectedImage({ src, id: `image-${index}` })}
+            initial={{ scale: 0.95, opacity: 0 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.4 }}>
+            <img
+              src={src}
+              alt={`Masonry ${index}`}
+              className="w-full rounded-lg"
+              loading="lazy"
+            />
+          </motion.div>
+        ))}
 
         <AnimatePresence>
           {selectedImage && (
             <motion.div
-              className="fixed inset-0 bg-black/70 bg-opacity-70 flex items-center justify-center z-50"
+              className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
